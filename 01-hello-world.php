0ad56ee2094630e354170f6c0212cfc5b79b02c8
nanupashaone <billy.tab@hotmail.com>
2022-10-05 11:49:12 +0200

    hello world

---
+++ 01-hello-world.php
@@ -0,0 +1,25 @@
+<?php
+
+// commentaire sur une ligne
+
+// on peut mettre des balises PHP n'importe où dans une page HTML
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello World</title>
+</head>
+<body>
+    <?php // ouverture de PHP
+
+    echo "<h1>Hello Word</h1>";
+
+  // fermeture de PHP  ?>
+</body>
+</html>
