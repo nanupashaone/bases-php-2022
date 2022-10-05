3a0337e5b31474451b2f38b97e44f13af86144bb
nanupashaone <billy.tab@hotmail.com>
2022-10-05 15:19:36 +0200

    exo billy

--- 03-variables-locales.php
+++ 03-variables-locales.php
@@ -40,6 +40,29 @@ Les noms de variables suivent les mêmes règles de nommage que les autres entit
     echo '$lala = ';
     echo "$lala <br>";
 
+    $_____coucou = 7;
+    echo '$_____coucou = ';
+    echo "$_____coucou <br>";
+
+    $bool = true;
+    echo '$bool = ';
+    echo "$bool <br>";
+    
+    $ILoveYou = "me too";
+    echo '$ILoveYou = ;';
+    echo "$ILoveYou <br>";
+
+    $mabapta = $lulu;
+    echo '$mabapta = ';
+    echo "$mabapta";
+
+
+   
+    
+    
+    
+    
+    
 
 ?>
 </body>
